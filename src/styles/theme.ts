85f4d7020dc935e3c7ef1fa63e6d20fa7b8897a8
mateusfg7 <mateusfelipefg77@gmail.com>
2020-12-08 10:50:51 -0300

    update theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,7 +3,11 @@ const defaultTheme = {
     background: '#fff',
     text: '#111',
     secondText: '#444',
-    primary: '#0070f3'
+    primary: '#0070f3',
+    unselected: {
+      text: '#444',
+      secondText: '#666'
+    }
   }
 }
 
