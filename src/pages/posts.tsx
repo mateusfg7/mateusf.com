4169ae45fcf1910cfbb9356a6a1cb681e300f678
mateusfg7 <mateusfelipefg77@gmail.com>
2020-12-08 10:51:36 -0300

    finish posts page

--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -1,16 +1,29 @@
 import React from 'react'
 import { GetStaticProps } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
 
 import Container from '../components/Container/index'
 import Header from '../components/Header/index'
 import Main from '../components/Main/index'
+import Date from '../components/Date'
+
+import { Post } from '../styles/pages/posts'
+
+import { getSortedPostsData } from '../lib/posts'
 
 interface Props {
   avatarUrl: string
+  allPostData: {
+    date: string
+    title: string
+    category: string
+    tags: string
+    id: string
+  }[]
 }
 
-const Posts: React.FC<Props> = props => {
+const Posts: React.FC<Props> = ({ avatarUrl, allPostData }) => {
   return (
     <div>
       <Head>
@@ -18,9 +31,22 @@ const Posts: React.FC<Props> = props => {
       </Head>
 
       <Container>
-        <Header imageUrl={props.avatarUrl} title="Posts" />
+        <Header imageUrl={avatarUrl} title="Posts" />
         <Main>
-          <h1>Posts</h1>
+          {allPostData.map((post, key) => {
+            return (
+              <Link key={key} href={`/posts/${post.id}`}>
+                <a>
+                  <Post>
+                    <h2>{post.title}</h2>
+                    <span>
+                      <Date dateString={post.date} />
+                    </span>
+                  </Post>
+                </a>
+              </Link>
+            )
+          })}
         </Main>
       </Container>
     </div>
@@ -30,9 +56,12 @@ const Posts: React.FC<Props> = props => {
 export default Posts
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
+  const allPostData = getSortedPostsData()
+
   return {
     props: {
-      avatarUrl: 'https://avatars1.githubusercontent.com/u/40613276?v=4'
+      avatarUrl: 'https://avatars1.githubusercontent.com/u/40613276?v=4',
+      allPostData
     }
   }
 }
---
+++ src/styles/pages/posts.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+export const Post = styled.section`
+  margin-bottom: 30px;
+
+  h2 {
+    font-size: 1.2rem;
+    color: ${props => props.theme.colors.unselected.text};
+  }
+
+  span {
+    color: ${props => props.theme.colors.unselected.secondText};
+  }
+
+  &:hover {
+    h2 {
+      /* color: ${props => props.theme.colors.text}; */
+      color: ${props => props.theme.colors.primary};
+    }
+
+    span {
+      color: ${props => props.theme.colors.secondText};
+    }
+  }
+`
