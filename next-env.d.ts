07389af54b73e7c09c5d4f50fff57e8996e65dae
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-03-25 14:34:34 -0300

    create global interface: PostData

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,10 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+interface PostData {
+  date: string
+  title: string
+  category: string
+  tags: string
+  id: string
+}
