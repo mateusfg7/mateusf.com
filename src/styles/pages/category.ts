efbc7c9db69823557996ad70ba2f861308c6f407
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-03-25 14:31:17 -0300

    create styles for category page

---
+++ src/styles/pages/category.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+export const Post = styled.section`
+  margin-top: 30px;
+
+  h2 {
+    font-size: 1.2rem;
+    color: ${props => props.theme.colors.unselected.text};
+  }
+
+  span {
+    color: ${props => props.theme.colors.unselected.secondText};
+  }
+
+  &:hover {
+    h2 {
+      color: ${props => props.theme.colors.primary};
+    }
+
+    span {
+      color: ${props => props.theme.colors.secondText};
+    }
+  }
+`
