a455d63cf918d3941e8298b68909ee6250ca89b7
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-03-25 15:48:00 -0300

    update Container width

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,8 +1,7 @@
 import styled from 'styled-components'
 
 const Container = styled.div`
-  /* border: 1px solid black; */
-  max-width: 1100px;
+  max-width: 900px;
   padding: 20px;
 
   margin: auto;
