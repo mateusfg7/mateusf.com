dba86ce69a663f05fcfa6f04f297582896278dc3
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-05-18 10:56:01 -0300

    import icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,6 +20,7 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700&display=swap"
             rel="stylesheet"
           />
+          <link rel="shortcut icon" href="assets/brain.png" type="image/png" />
         </Head>
         <body>
           <Main />
