82ae9f4920e418e6589aa99caaee2709abdf8e05
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-06-11 10:24:57 -0300

    fix #33

--- new-post.sh
+++ new-post.sh
@@ -14,7 +14,7 @@ tags: 'title4test, content4test, code4test'
 
 echo "---" > $file
 echo "title: 'Draft Post'" >> $file
-echo "date '$current_date'" >> $file
+echo "date: '$current_date'" >> $file
 echo "description: 'Post made for test markdown render'" >> $file
 echo "category: 'Draft'" >> $file
 echo "tags: 'title4test, content4test, code4test'" >> $file
