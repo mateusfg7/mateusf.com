e12ca6e5baf7329484f71fda9c8ab9fd55ed762a
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-06-11 10:26:16 -0300

    clean new-post script

--- new-post.sh
+++ new-post.sh
@@ -2,15 +2,6 @@
 
 current_date=$(date '+%Y-%m-%d')
 file="_posts/$1.md"
-metadata="
----
-title: 'Draft Post'
-date: '$current_date'
-description: 'Post made for test markdown render'
-category: 'Draft'
-tags: 'title4test, content4test, code4test'
----
-"
 
 echo "---" > $file
 echo "title: 'Draft Post'" >> $file
