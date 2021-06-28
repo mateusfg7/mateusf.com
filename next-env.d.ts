9cc3dce42cff195e9329124036c4d05de7363bdb
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-06-28 13:33:10 -0300

    move PostData interface definition
    from next-env.d.ts
    to src/lib/types/categories.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,12 +1,2 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-
-interface PostData {
-  date: string
-  title: string
-  category: string
-  tags: string
-  id: string
-  description: string
-  lastUpdate?: string
-}
---
+++ src/lib/types/categories.d.ts
@@ -0,0 +1,9 @@
+interface PostData {
+  date: string
+  title: string
+  category: string
+  tags: string
+  id: string
+  description: string
+  lastUpdate?: string
+}
