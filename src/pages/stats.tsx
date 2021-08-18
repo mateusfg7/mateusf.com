a586a2df88de6226a0738e862c0bf13dd2355a07
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-08-18 11:08:20 -0300

    update Time Code stats url

--- src/pages/stats.tsx
+++ src/pages/stats.tsx
@@ -34,7 +34,7 @@ const Stats: React.FC = () => {
           />
           <StatsTopic
             title="Time Coding"
-            imgSrc="https://github-readme-stats.vercel.app/api/wakatime?username=mateusfg7&layout=compact&hide_title=true&hide_border=true&show_icons=true&title_color=000&icon_color=555&text_color=000"
+            imgSrc="https://github-readme-stats.vercel.app/api/wakatime?username=mateusfg7&layout=compact&hide_title=true&hide_border=true&show_icons=true&title_color=000&icon_color=555&text_color=000&api_domain=wakapi.dev"
             gridArea="code-time"
           />
           <StatsTopic
