79c177042c050770da795f0b991e58aee1be1762
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-02-01 17:23:40 -0300

    forma unified content processor lib

--- src/lib/unifiedContentProcessor.ts
+++ src/lib/unifiedContentProcessor.ts
@@ -1,52 +1,32 @@
 import React from 'react'
 
 import { unified } from 'unified'
-import markdown from 'remark-parse'
-import rehypeHighlight from 'rehype-highlight'
-import math from 'remark-math'
-import katex from 'rehype-katex'
-import remark2rehype from 'remark-rehype'
-import rehype2react from 'rehype-react'
-import gfm from 'remark-gfm'
-import toc from 'rehype-toc'
-import slug from 'rehype-slug'
-import rehypeRaw from 'rehype-raw'
 
-import elixir from 'highlight.js/lib/languages/elixir'
-import vim from 'highlight.js/lib/languages/vim'
-import typescript from 'highlight.js/lib/languages/typescript'
-import javascript from 'highlight.js/lib/languages/javascript'
-import bash from 'highlight.js/lib/languages/bash'
-import python from 'highlight.js/lib/languages/python'
-import css from 'highlight.js/lib/languages/css'
-import xml from 'highlight.js/lib/languages/xml'
-import plaintext from 'highlight.js/lib/languages/plaintext'
-import sql from 'highlight.js/lib/languages/sql'
+import remarkParse from 'remark-parse'
+import remarkGfm from 'remark-gfm'
+import remarkMath from 'remark-math'
+import remark2rehype from 'remark-rehype'
+
+import rehypeRaw from 'rehype-raw'
+import rehypeHighlight from 'rehype-highlight'
+import rehypeKatex from 'rehype-katex'
+import rehypeSlug from 'rehype-slug'
+import rehypeToc from 'rehype-toc'
+import rehype2react from 'rehype-react'
+
+import elixir from 'highlight.js/lib/languages/elixir.js'
 
 export function unifiedContentProcessor(contentToProcess: string) {
-  const highlightLanguages = {
-    elixir,
-    vim,
-    typescript,
-    javascript,
-    bash,
-    python,
-    css,
-    xml,
-    plaintext,
-    sql
-  }
-
   const contentProcessor = unified()
-    .use(markdown)
-    .use(gfm)
-    .use(math)
+    .use(remarkParse)
+    .use(remarkGfm)
+    .use(remarkMath)
     .use(remark2rehype, { allowDangerousHtml: true })
     .use(rehypeRaw)
-    .use(rehypeHighlight, { languages: highlightLanguages })
-    .use(katex)
-    .use(slug)
-    .use(toc)
+    .use(rehypeHighlight, { languages: { elixir } })
+    .use(rehypeKatex)
+    .use(rehypeSlug)
+    .use(rehypeToc)
     .use(rehype2react, { createElement: React.createElement })
 
   return contentProcessor.processSync(contentToProcess).result
