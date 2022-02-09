c3d47a5e0cc5a9ce28143e76cafd4f7fdaedcd9a
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-02-09 12:53:53 -0300

    add umami config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Head from 'next/head'
 import { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 
@@ -7,10 +8,20 @@ import defaultTheme from '../styles/theme'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Component {...pageProps} />
-      <GlobalStyle />
-    </ThemeProvider>
+    <>
+      <Head>
+        <script
+          async
+          defer
+          data-website-id="39d18d62-74c2-4bab-a2f8-a346b63d55b5"
+          src="https://mateusfg7-umami-instance.herokuapp.com/umami.js"
+        />
+      </Head>
+      <ThemeProvider theme={defaultTheme}>
+        <Component {...pageProps} />
+        <GlobalStyle />
+      </ThemeProvider>
+    </>
   )
 }
 
