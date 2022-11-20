aec27ab08a87bbfcc15dade5e210586ed1ba9314
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-11-20 20:43:33 -0300

    refactor: convert Header component
    * Delete `styles.ts` with styled-components codes
    * Use tailwind classes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
 
-import { StyledHeader } from './styles'
-
 interface Props {
   imageUrl: string
   title: string
@@ -11,27 +9,29 @@ interface Props {
 
 const Header: React.FC<Props> = ({ imageUrl, title, knowledgePage }) => {
   return (
-    <StyledHeader>
-      <div>
-        <img src={imageUrl} alt="" />
-        <h2>brain</h2>/<h1>{title}</h1>
+    <header className="flex justify-between items-center mb-24">
+      <div className="flex items-center gap-5">
+        <img src={imageUrl} className="w-12 h-12 rounded-full text-gray-600" />
+        <h2>brain</h2>
+        <span className="text-xs">/</span>
+        <h1 className="text-xl font-bold">{title}</h1>
       </div>
       <nav>
         {knowledgePage ? (
           <Link href="/">
-            <a>Home</a>
+            <a className="text-[#111] mr-8 hover:underline">Home</a>
           </Link>
         ) : (
           <Link href="/knowledge">
-            <a>Knowledge</a>
+            <a className="text-[#111] mr-8 hover:underline">Knowledge</a>
           </Link>
         )}
 
         <Link href="/categories">
-          <a>Categories</a>
+          <a className="text-[#111] mr-8 hover:underline">Categories</a>
         </Link>
       </nav>
-    </StyledHeader>
+    </header>
   )
 }
 
