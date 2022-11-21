0446bb40e16e39febddb7d26b63808bb5101a239
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-11-21 13:53:14 -0300

    feat(tailwind): add new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['./src/**/*.{ts,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        background: '#fff',
+        text: '#111',
+        secondText: '#444',
+        primary: '#0070f3',
+        unselectedText: '#444',
+        unselectedSecondText: '#666'
+      }
+    }
   },
   plugins: []
 }
