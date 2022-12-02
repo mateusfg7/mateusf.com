e6ed864928b35e864750e7ad968cde68e77aa721
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-12-02 02:26:51 -0300

    feat(mdx): increment rehype-pretty-code options

--- src/lib/rehypePrettyCode.ts
+++ src/lib/rehypePrettyCode.ts
@@ -1,5 +1,19 @@
 import { type Options } from 'rehype-pretty-code'
 
 export const rehypePrettyCodeOptions: Partial<Options> = {
-  theme: 'github-light'
+  theme: 'github-light',
+  onVisitLine(node) {
+    // Prevent lines from collapsing in `display: grid` mode, and
+    // allow empty lines to be copy/pasted
+    if (node.children.length === 0) {
+      node.children = [{ type: 'text', value: ' ' }]
+    }
+  },
+  // Feel free to add classNames that suit your docs
+  onVisitHighlightedLine(node) {
+    node.properties.className.push('highlighted')
+  },
+  onVisitHighlightedWord(node) {
+    node.properties.className = ['word']
+  }
 }
