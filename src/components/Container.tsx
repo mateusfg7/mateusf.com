fbec0c323ea0d950c9236b22ff00cd287858560c
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-12-10 11:30:42 -0300

    fix: type error on `Container` component
    Type error: Property "children" does not exist on type "{}"
    
    fix #414

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,5 +1,9 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 
-export const Container: React.FC = ({ children }) => {
+interface Props {
+  children: ReactNode
+}
+
+export const Container = ({ children }: Props) => {
   return <div className="max-w-4xl p-5 m-auto">{children}</div>
 }
