fb20dd0ed644264c4725e81a7ec9d837573bee41
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-12-12 09:52:13 -0300

    feat: convert Document to functional component
    Convert custom Document from class to react fc

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,38 +1,36 @@
 import React from 'react'
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document'
 
-export default class MyDocument extends Document {
-  render(): JSX.Element {
-    return (
-      <Html lang="en">
-        <Head>
-          <meta charSet="utf-8" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;600&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@200;300;400;600;800;900&display=swap"
-            rel="stylesheet"
-          />
-          <link rel="shortcut icon" href="assets/brain.png" type="image/png" />
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta charSet="utf-8" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;600&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@200;300;400;600;800;900&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="shortcut icon" href="assets/brain.png" type="image/png" />
 
-          <link
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/katex/dist/katex.css"
-            crossOrigin="anonymous"
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/katex/dist/katex.css"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
