e66a3baae54f3c0c8ef96566c4dd8e45e4c6aff6
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-12-12 11:32:35 -0300

    feat(ui): add hover background on category list

--- src/pages/categories.tsx
+++ src/pages/categories.tsx
@@ -27,7 +27,7 @@ const Categories: React.FC<Props> = ({ categories }) => {
           <div className="flex flex-wrap gap-2">
             {categories.map((categoryData, index) => (
               <Link key={index} href={`/category/${categoryData.category}`}>
-                <div className="py-1 px-4 border-2 border-solid border-neutral-700 dark:border-neutral-500 hover:border-blue-700 dark:hover:border-blue-500 font-bold text-neutral-700 dark:text-neutral-500 hover:text-blue-700 dark:hover:text-blue-500 duration-100 hover:cursor-pointer">
+                <div className="py-1 px-4 border-2 border-solid border-neutral-700 dark:border-neutral-500 hover:border-blue-700 dark:hover:border-blue-500 font-bold text-neutral-700 dark:text-neutral-500 hover:text-blue-700 dark:hover:text-blue-500  hover:bg-blue-700/10 dark:hover:bg-blue-500/10 duration-100 hover:cursor-pointer">
                   <p>
                     {categoryData.category}
                     <sup> {categoryData.numberOfPosts} </sup>
