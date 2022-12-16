31ff389584fc77eddaf7ebb0172fa75a6814b095
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-12-16 16:45:54 -0300

    feat(seo): add meta tags on `_document.tsx`

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,8 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700&display=swap"
