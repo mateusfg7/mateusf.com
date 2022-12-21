531927364c3f7dbde55bb59dd8405cc53f8ca8f8
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-12-21 17:46:52 -0300

    feat(ui): add icons on ContactSection

--- src/components/portifolio/ContactSection.tsx
+++ src/components/portifolio/ContactSection.tsx
@@ -1,3 +1,10 @@
+import {
+  AiOutlineGithub,
+  AiOutlineLinkedin,
+  AiOutlineMail,
+  AiOutlineWhatsApp
+} from 'react-icons/ai'
+
 export function ContactSection() {
   return (
     <div
@@ -10,40 +17,56 @@ export function ContactSection() {
         </div>
         <div className="flex justify-center flex-wrap gap-10">
           <div className="flex flex-col items-center gap-6 border border-neutral-800 rounded-3xl p-12">
-            <h2 className="text-xl text-green-300">[X] Whatsapp</h2>
+            <h2 className="text-xl text-green-300 flex items-center gap-2">
+              <AiOutlineWhatsApp /> Whatsapp
+            </h2>
             <span className="text-lg">+55 (37) 99844-0073</span>
             <a
-              href="#"
+              href="https://wa.me/+5537998440073"
+              target="_blank"
+              rel="noreferrer"
               className="text-green-300 border border-green-300 p-3 rounded-xl hover:bg-green-300/10"
             >
               Enviar mensagem
             </a>
           </div>
           <div className="flex flex-col items-center gap-6 border border-neutral-800 rounded-3xl p-12">
-            <h2 className="text-xl text-red-300">[X] Email</h2>
+            <h2 className="text-xl text-red-300 flex items-center gap-2">
+              <AiOutlineMail /> Email
+            </h2>
             <span className="text-lg">mateusfelipefg77@gmail.com</span>
             <a
-              href="#"
+              href="mailto:mateusfelipefg77@gmail.com"
+              target="_blank"
+              rel="noreferrer"
               className="text-red-300 border border-red-300 p-3 rounded-xl hover:bg-red-300/10"
             >
               Enviar mensagem
             </a>
           </div>
           <div className="flex flex-col items-center gap-6 border border-neutral-800 rounded-3xl p-12">
-            <h2 className="text-xl text-sky-500">[X] Linkedin</h2>
+            <h2 className="text-xl text-sky-500 flex items-center gap-2">
+              <AiOutlineLinkedin /> Linkedin
+            </h2>
             <span className="text-lg">linkedin.com/in/mateusfg</span>
             <a
-              href="#"
+              href="https://linkedin.com/in/mateusfg"
+              target="_blank"
+              rel="noreferrer"
               className="text-sky-500 border border-sky-500 p-3 rounded-xl hover:bg-sky-500/10"
             >
               Visualizar perfil
             </a>
           </div>
           <div className="flex flex-col items-center gap-6 border border-neutral-800 rounded-3xl p-12">
-            <h2 className="text-xl text-stone-200">[X] Github</h2>
+            <h2 className="text-xl text-stone-200 flex items-center gap-2">
+              <AiOutlineGithub /> Github
+            </h2>
             <span className="text-lg">github.com/mateusfg7</span>
             <a
-              href="#"
+              href="https://github.com/mateusfg7"
+              target="_blank"
+              rel="noreferrer"
               className="text-stone-200 border border-stone-200 p-3 rounded-xl hover:bg-stone-200/10"
             >
               Visualizar perfil
