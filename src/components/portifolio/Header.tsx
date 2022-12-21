50f18209f00786b213c379c3d007f0aa0eef20cd
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-12-21 16:44:16 -0300

    feat(ui): add icons and links on portifolio/Header

--- src/components/portifolio/Header.tsx
+++ src/components/portifolio/Header.tsx
@@ -1,9 +1,11 @@
+import { FiGithub, FiInstagram, FiLinkedin } from 'react-icons/fi'
+
 export function Header() {
   return (
     <div className="flex justify-center items-center fixed bg-neutral-1000/90 backdrop-blur-xl w-full">
       <nav className="content-w flex items-center justify-center py-6 relative">
         <div className=" absolute left-0">
-          <a className="text-blue-500 font-bold" href="#home">
+          <a className="text-blue-500 font-medium font-chivo-mono" href="#home">
             Mateus Felipe
           </a>
         </div>
@@ -34,9 +36,33 @@ export function Header() {
           </a>
         </div>
         <div className="flex items-center gap-4 absolute right-0">
-          <div title="Linkedin">[X]</div>
-          <div title="Github">[X]</div>
-          <div title="Instagram">[X]</div>
+          <a
+            className="text-xl text-neutral-300 hover:text-neutral-50 hover:cursor-pointer"
+            href="https://linkedin.com/in/mateusfg"
+            target="_blank"
+            rel="noreferrer"
+            title="Linkedin"
+          >
+            <FiLinkedin />
+          </a>
+          <a
+            className="text-xl text-neutral-300 hover:text-neutral-50 hover:cursor-pointer"
+            href="https://github.com/mateusfg7"
+            target="_blank"
+            rel="noreferrer"
+            title="Github"
+          >
+            <FiGithub />
+          </a>
+          <a
+            className="text-xl text-neutral-300 hover:text-neutral-50 hover:cursor-pointer"
+            href="https://instagram.com/mateusfg7"
+            target="_blank"
+            rel="noreferrer"
+            title="Instagram"
+          >
+            <FiInstagram />
+          </a>
         </div>
       </nav>
     </div>
