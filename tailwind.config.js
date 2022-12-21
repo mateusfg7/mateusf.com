a1f8c6b1de08273119090a83b8553f6ef097103e
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-12-21 15:32:06 -0300

    chore(tailwind): extend default theme
    * Add new color
    * Add new animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,31 @@ module.exports = {
   content: ['./src/**/*.{ts,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        neutral: {
+          1000: 'rgba(17, 17, 17)'
+        }
+      },
+      animation: {
+        cursor: 'cursor .6s linear infinite alternate',
+        typing: 'typing 1.8s ease-in .8s 1 normal both',
+      },
+      keyframes: {
+        typing: {
+          from: {
+            width: 0
+          },
+          to: {
+            width: '22.7rem'
+          }
+        },
+        cursor: {
+          '0%, 40%': { opacity: 1 },
+          '60%, 100%': { opacity: 0 }
+        }
+      }
+    }
   },
   plugins: []
 }
