1bccd122418f7cba99288c550becc04dbb78f201
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-12-22 12:57:49 -0300

    feat: add projects

--- src/components/portifolio/projects.tsx
+++ src/components/portifolio/projects.tsx
@@ -1,3 +1,15 @@
+import { BsBuilding } from 'react-icons/bs'
+import {
+  SiTypescript,
+  SiReact,
+  SiExpo,
+  SiNextdotjs,
+  SiTailwindcss,
+  SiVite,
+  SiNodedotjs,
+  SiPrisma
+} from 'react-icons/si'
+
 export interface ProjectData {
   title: string
   image?: string
@@ -14,79 +26,219 @@ export interface ProjectData {
 
 export const devProjects: ProjectData[] = [
   {
-    title: 'Noisekun',
-    image: 'https://github.com/mateusfg7.png',
+    title: 'Formatch',
+    image:
+      'https://raw.githubusercontent.com/mateusfg7/formatch/main/.github/thumbnail.png',
     core_techs: [
       {
         key: 'typescript',
-        icon: () => <span>[X]</span>
+        icon: () => (
+          <SiTypescript className="hover:text-[#358EF1]" title="typescript" />
+        )
       },
       {
-        key: 'nextjs',
-        icon: () => <span>[X]</span>
+        key: 'react-native',
+        icon: () => (
+          <SiReact className="hover:text-[#61DBFB]" title="react-native" />
+        )
       },
       {
-        key: 'tailwind',
-        icon: () => <span>[X]</span>
+        key: 'expo',
+        icon: () => <SiExpo title="expo" />
       }
     ],
     description:
-      'Mateus felipe gonçalves mateus felipe gonçalves mateus felipe gonçalves mateus felipe gonçalves mateus felipe gonçalves mateus felipe gonçalves mateus felipe gonçalves.',
+      'Aplicativo que conecta você a um profissional da área de construção civil, e ainda te mantém informado sobre novidades do setor. ',
     tags: [
-      'tailwind',
-      'nextjs',
-      'howler.js',
       'typescript',
-      'javascript',
-      'vercel'
+      'react-native',
+      'expo',
+      'native-base',
+      'phosphor-icons'
     ],
+    repository: 'https://github.com/mateusfg7/formatch'
+  },
+  {
+    title: 'Noisekun',
+    image:
+      'https://raw.githubusercontent.com/mateusfg7/Noisekun/main/.github/assets/thumbnail.png',
+    core_techs: [
+      {
+        key: 'typescript',
+        icon: () => (
+          <SiTypescript className="hover:text-[#358EF1]" title="typescript" />
+        )
+      },
+      {
+        key: 'reactjs',
+        icon: () => <SiReact className="hover:text-[#61DBFB]" title="reactjs" />
+      },
+      {
+        key: 'nextjs',
+        icon: () => <SiNextdotjs title="nextjs" />
+      },
+      {
+        key: 'tailwindcss',
+        icon: () => (
+          <SiTailwindcss className="hover:text-[#38BDF8]" title="tailwindcss" />
+        )
+      }
+    ],
+    description:
+      'Web page for listen combinations of sounds for relaxing or getting more productive on tasks.',
+    tags: ['typescript', 'reactjs', 'nextjs', 'tailwindcss', 'zustand', 'pwa'],
     repository: 'https://github.com/mateusfg7/Noisekun',
     website: 'https://noisekun.vercel.app'
   },
   {
-    title: 'Noisekun',
-    image: 'https://github.com/mateusfg7.png',
+    title: 'LMJ Automotive',
+    image:
+      'https://raw.githubusercontent.com/mateusfg7/lmj-automotive/main/.github/thumbnail.png',
     core_techs: [
       {
         key: 'typescript',
-        icon: () => <span>[X]</span>
+        icon: () => (
+          <SiTypescript className="hover:text-[#358EF1]" title="typescript" />
+        )
+      },
+      {
+        key: 'reactjs',
+        icon: () => <SiReact className="hover:text-[#61DBFB]" title="react" />
       },
       {
         key: 'nextjs',
-        icon: () => <span>[X]</span>
+        icon: () => <SiNextdotjs title="nextjs" />
       },
       {
-        key: 'tailwind',
-        icon: () => <span>[X]</span>
+        key: 'tailwindcss',
+        icon: () => (
+          <SiTailwindcss className="hover:text-[#38BDF8]" title="tailwindcss" />
+        )
       }
     ],
     description:
-      'Mateus felipe gonçalves mateus felipe gonçalves mateus felipe gonçalves mateus felipe gonçalves mateus felipe gonçalves mateus felipe gonçalves mateus felipe gonçalves.',
+      'Website da LMJ Automotive, uma oficina mecânica que oferece serviço móvel. Empresa fictícia para o Projeto Final do curso de Aprendizado Industrial em Logística, na instituição SESI SENAI Dr. Celso Charuri - Pará de Minas MG.',
     tags: [
-      'tailwind',
-      'nextjs',
-      'howler.js',
       'typescript',
-      'javascript',
-      'vercel'
+      'reactjs',
+      'nextjs',
+      'tailwindcss',
+      'react-icons',
+      'tailwind-styled-components'
     ],
-    repository: 'https://github.com/mateusfg7/Noisekun',
-    website: 'https://noisekun.vercel.app'
+    repository: 'https://github.com/mateusfg7/lmj-automotive',
+    website: 'https://www.lmjautomotive.com.br/'
+  },
+  {
+    title: 'Feedget',
+    image:
+      'https://raw.githubusercontent.com/mateusfg7/nlw-return/main/.github/thumbnail.png',
+    core_techs: [
+      {
+        key: 'typescript',
+        icon: () => (
+          <SiTypescript className="hover:text-[#358EF1]" title="typescript" />
+        )
+      },
+      {
+        key: 'reactjs',
+        icon: () => <SiReact className="hover:text-[#61DBFB]" title="reactjs" />
+      },
+      {
+        key: 'vite',
+        icon: () => <SiVite className="hover:text-[#FFC119]" title="vitejs" />
+      },
+      {
+        key: 'nodejs',
+        icon: () => (
+          <SiNodedotjs className="hover:text-[#66CC33]" title="nodejs" />
+        )
+      },
+      {
+        key: 'prisma',
+        icon: () => <SiPrisma className="hover:text-[#4C51BF]" title="prisma" />
+      }
+    ],
+    description:
+      'Feedget is widget to send feedbacks to the page owner. This project was made during the Next Level Week Return: Impulse, by Rocketseat. ',
+    tags: [
+      'typescript',
+      'reactjs',
+      'vite',
+      'tailwindcss',
+      'headlessui',
+      'nodejs',
+      'prisma',
+      'jest',
+      'nodemailer'
+    ],
+    repository: 'https://github.com/mateusfg7/nlw-return'
+  },
+  {
+    title: 'Bolão da Copa',
+    image:
+      'https://raw.githubusercontent.com/mateusfg7/nlw-copa/main/.github/thumbnail.png',
+    core_techs: [
+      {
+        key: 'typescript',
+        icon: () => (
+          <SiTypescript className="hover:text-[#358EF1]" title="typescript" />
+        )
+      },
+      {
+        key: 'reactjs',
+        icon: () => <SiReact className="hover:text-[#61DBFB]" title="reactjs" />
+      },
+      {
+        key: 'nextjs',
+        icon: () => <SiNextdotjs title="nextjs" />
+      },
+      {
+        key: 'nodejs',
+        icon: () => (
+          <SiNodedotjs className="hover:text-[#66CC33]" title="nodejs" />
+        )
+      },
+      {
+        key: 'prisma',
+        icon: () => <SiPrisma className="hover:text-[#4C51BF]" title="prisma" />
+      },
+      {
+        key: 'expo',
+        icon: () => <SiExpo title="expo" />
+      }
+    ],
+    description:
+      'App para criar palpites de resultados dos jogos da FIFA World Cup 2022. Desenvolvido durante a NLW Copa, na semana do dia 31 ao dia 05 de novembro de 2022. by @Rocketseat',
+    tags: [
+      'typescript',
+      'reactjs',
+      'nextjs',
+      'tailwindcss',
+      'nodejs',
+      'prisma',
+      'fastify',
+      'react-native',
+      'expo'
+    ],
+    repository: 'https://github.com/mateusfg7/nlw-copa'
   }
 ]
 
 export const civilProjects: ProjectData[] = [
   {
     title: 'Planta Baixa',
+    image: 'https://i.imgur.com/Wpx9KQ6.jpg',
     core_techs: [
       {
         key: 'bricscad',
-        icon: () => <span>[X]</span>
+        icon: () => <BsBuilding />
       }
     ],
     description:
-      'Planta baixa de uma casa 300x300m realizado no curso Técnico em Edificações, na instituição SESI SENAI Dr. Celso Charuri, em Pará de Minas.',
-    tags: ['planta baixa', 'cortes', 'carimbo-prefeitura', 'bricscad'],
-    files: 'https://drive.google.com'
+      'Planta baixa e memorial descriptivo de uma casa com 360m², realizado no curso Técnico em Edificações na instituição SESI SENAI Dr. Celso Charuri, em Pará de Minas.',
+    tags: ['planta baixa', 'cortes', 'memorial descritivo', 'bricscad'],
+    files:
+      'https://drive.google.com/drive/folders/1z3W5wV5wnzTLP4F3XyNogceVfQxAm7aB?usp=share_link'
   }
 ]
