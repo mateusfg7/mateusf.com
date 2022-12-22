08398580af6d5d63ff15c960da35c0741374447f
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-12-22 16:24:43 -0300

    feat: change IDs on each portifolio section

--- src/components/portifolio/ProjectsSection.tsx
+++ src/components/portifolio/ProjectsSection.tsx
@@ -4,7 +4,7 @@ import { devProjects, civilProjects } from './projects'
 
 export function ProjectsSection() {
   return (
-    <div id="projects" className="min-h-screen flex justify-center py-20">
+    <div id="projetos" className="min-h-screen flex justify-center py-20">
       <div className="content-w">
         <div className="w-full text-center mb-20">
           <h1 className="text-blue-500 text-4xl">Projetos Pessoais</h1>
