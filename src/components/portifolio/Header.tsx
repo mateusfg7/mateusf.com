96d7650e7a436559ea046f9f9a71730c050fffa1
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-12-22 16:24:01 -0300

    feat(ui): update Header nav behavior
    * Add styles on menu item based on scroll position
    * Add smoth scroll to section

--- src/components/portifolio/Header.tsx
+++ src/components/portifolio/Header.tsx
@@ -1,36 +1,61 @@
 import { FiGithub, FiInstagram, FiLinkedin } from 'react-icons/fi'
 
-export function Header() {
+interface Props {
+  scrollPosition: number
+}
+
+export function Header({ scrollPosition }: Props) {
+  function scrollTo(position: number) {
+    if (window) window.scrollTo({ top: position, behavior: 'smooth' })
+  }
+
   return (
-    <div className="flex justify-center items-center fixed bg-neutral-1000/90 backdrop-blur-xl w-full">
+    <div className="flex justify-center items-center fixed bg-neutral-1000/80 backdrop-blur-lg w-full">
       <nav className="content-w flex items-center justify-center py-6 relative">
         <div className=" absolute left-0">
-          <a className="text-blue-500 font-medium font-chivo-mono" href="#home">
+          <a
+            className="text-blue-500 font-medium font-chivo-mono hover:cursor-pointer"
+            onClick={() => scrollTo(0)}
+          >
             Mateus Felipe
           </a>
         </div>
         <div className="flex items-center gap-4">
           <a
-            className="text-neutral-400 hover:text-neutral-50 hover:cursor-pointer"
-            href="#about"
+            className={`${
+              scrollPosition >= 590 && scrollPosition < 1380
+                ? 'text-neutral-50'
+                : 'text-neutral-400'
+            } hover:text-neutral-50 hover:cursor-pointer`}
+            onClick={() => scrollTo(768)}
           >
             Sobre mim
           </a>
           <a
-            className="text-neutral-400 hover:text-neutral-50 hover:cursor-pointer"
-            href="#projects"
+            className={`${
+              scrollPosition >= 1380 && scrollPosition < 4140
+                ? 'text-neutral-50'
+                : 'text-neutral-400'
+            } hover:text-neutral-50 hover:cursor-pointer`}
+            onClick={() => scrollTo(1536)}
           >
             Projetos
           </a>
           <a
-            className="text-neutral-400 hover:text-neutral-50 hover:cursor-pointer"
-            href="#knowledge"
+            className={`${
+              scrollPosition >= 4140 && scrollPosition < 5382
+                ? 'text-neutral-50'
+                : 'text-neutral-400'
+            } hover:text-neutral-50 hover:cursor-pointer`}
+            onClick={() => scrollTo(4544)}
           >
             Conhecimentos
           </a>
           <a
-            className="text-neutral-400 hover:text-neutral-50 hover:cursor-pointer"
-            href="#contact"
+            className={`${
+              scrollPosition >= 5382 ? 'text-neutral-50' : 'text-neutral-400'
+            } hover:text-neutral-50 hover:cursor-pointer`}
+            onClick={() => scrollTo(5661)}
           >
             Contato
           </a>
