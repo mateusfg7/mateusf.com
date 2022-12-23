020d031c10090425588ebd1f8719a292f16e529e
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-12-23 09:37:31 -0300

    fix: scroll behavior on portifolio'

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className="scroll-smooth">
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
