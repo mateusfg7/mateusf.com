f0127c7d2f4da0834b10f6f35c7a8af812d31c13
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-03-28 11:29:43 -0300

    refactor(app): move tags page to app dir

---
+++ src/app/(blog)/tag/[tag]/page.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import type { Metadata } from 'next'
+
+import { Container } from 'components/Container'
+import { Header } from 'components/Header'
+import { KnowledgeList } from 'components/KnowledgeList'
+
+import { getUniqueTagListFromPosts, getPostListBasedOnTag } from 'lib/tags'
+import { getSortedPosts } from 'lib/getSortedPosts'
+
+interface Props {
+  params: { tag: string }
+}
+
+export function generateMetadata({ params }: Props): Metadata {
+  return {
+    title: `mfg-b | ${params.tag}`,
+    description: `Posts with tag ${params.tag}`
+  }
+}
+
+export default function Page({ params }: Props) {
+  const { tag } = params
+  const postList = getSortedPosts(getPostListBasedOnTag(tag))
+
+  return (
+    <div>
+      <Container>
+        <Header title={tag} isMainTitle />
+        <main>
+          <KnowledgeList posts={postList} />
+        </main>
+      </Container>
+    </div>
+  )
+}
+
+export async function generateStaticParams() {
+  const tagList = getUniqueTagListFromPosts()
+
+  return tagList.map(tag => ({ tag }))
+}
