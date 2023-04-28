fdb0f5666400121208abdc34dc3899c8362cba1e
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-04-28 19:00:59 -0300

    feat(ui/ux): made Header responseive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -36,7 +36,7 @@ export function Header() {
     )
   }, [scrollPosition, maxScrollValue])
 
-  const isOnTop = percentScrollPosition > 0
+  const isNotOnTop = percentScrollPosition > 0
 
   const MenuItem: React.FC<{ name: string; path: string }> = ({
     name,
@@ -52,26 +52,26 @@ export function Header() {
 
   return (
     <header
-      className={`w-full fixed top-0 bg-neutral-100/80 dark:bg-neutral-1000/80 backdrop-blur-lg ${
-        isOnTop ? 'py-3' : 'py-6'
-      } duration-300 border-b ${
-        isOnTop
-          ? 'border-b-neutral-50 dark:border-b-neutral-800'
-          : 'border-b-transparent'
+      className={`md:w-full fixed top-3 left-3 right-3 md:top-0 md:right-0 md:left-0 rounded-xl md:rounded-none bg-neutral-100/80 dark:bg-neutral-1000/80 backdrop-blur-lg ${
+        isNotOnTop ? 'py-3' : 'py-3 md:py-6'
+      } duration-300 border border-neutral-50 dark:border-neutral-800 md:border-none md:border-b ${
+        isNotOnTop
+          ? 'md:border-b-neutral-50 md:dark:border-b-neutral-800'
+          : 'md:border-b-transparent'
       }`}
     >
-      <div className="blog-content-w m-auto flex justify-between items-center">
+      <div className="blog-content-w m-auto flex flex-col md:flex-row justify-between items-center flex-wrap">
         <Link href="/">
           <h1
             className={`text-blue-700 dark:text-blue-500 ${
-              isOnTop ? 'font-medium' : 'font-base'
+              isNotOnTop ? 'font-medium' : 'font-base'
             } font-chivo-mono hover:cursor-pointer`}
           >
             Mateus Felipe
           </h1>
         </Link>
-        <div className="flex justify-center items-center gap-8">
-          <nav className="flex justify-center items-center gap-5">
+        <div className="flex justify-center items-center gap-8 flex-wrap">
+          <nav className="flex justify-center items-center gap-5 flex-wrap">
             <MenuItem name="Home" path="/" />
             <MenuItem name="Categories" path="/categories" />
             <MenuItem name="Portifolio" path="/portifolio" />
