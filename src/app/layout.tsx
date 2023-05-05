6c7711a7f0d6259022203172a3c889d48a7b53b4
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-05-05 09:38:53 -0300

    fix(app/layout): use `crossOrigin` as "anonymous"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
-          crossOrigin="true"
+          crossOrigin="anonymous"
         />
         <link
           href="https://fonts.googleapis.com/css2?family=Chivo+Mono:wght@500&family=Source+Sans+Pro:wght@300;400;600&family=Inter:wght@200;300;400;500;600;700&display=swap"
