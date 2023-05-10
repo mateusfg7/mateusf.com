9d16c5ece058f7f71217e96a5c78a1ee27cca096
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-05-10 23:40:05 +0000

    feat(ui): show test message instead of "1" on year section
    resolve #487

--- src/components/KnowledgeList.tsx
+++ src/components/KnowledgeList.tsx
@@ -16,7 +16,7 @@ export function KnowledgeList({ posts, separateByYear = false }: Props) {
         {postsByYear.map(postsOfYear => (
           <div key={postsOfYear.year}>
             <h1 className="mb-5 font-bold text-2xl p-3 rounded-xl bg-neutral-50 dark:bg-neutral-900">
-              {postsOfYear.year}
+              {postsOfYear.year === 1 ? 'Posts for test' : postsOfYear.year}
             </h1>
             <div className="flex flex-col gap-3">
               {postsOfYear.posts.map((post, key) => (
