98b38af9de1984fe6d5eb7a73660eecec91c96de
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-05-14 16:09:28 +0000

    feat(contentlayer): add remote files function

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-void */
 import remarkGfm from 'remark-gfm'
 import remarkMath from 'remark-math'
 import remarkBreaks from 'remark-breaks'
@@ -9,7 +10,8 @@ import rehypeKatex from 'rehype-katex'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import rehypeShiftHeading from 'rehype-shift-heading'
 
-import { makeSource } from 'contentlayer/source-files'
+import { spawn } from 'node:child_process'
+import { makeSource } from 'contentlayer/source-remote-files'
 
 import { Post } from './content/definitions/Post'
 import {
@@ -18,8 +20,63 @@ import {
   rehypeShiftHeadingOptions
 } from './content/plugin'
 
+const syncContentFromGit = async (contentDir: string) => {
+  const syncRun = async () => {
+    const gitUrl = 'https://github.com/mateusfg7/_mfg-b_articles'
+    await runBashCommand(`
+      if [ -d  "${contentDir}" ];
+        then
+          cd "${contentDir}"; git pull;
+        else
+          git clone --depth 1 --single-branch ${gitUrl} ${contentDir}/posts;
+      fi
+    `)
+  }
+
+  let wasCancelled = false
+  let syncInterval
+
+  const syncLoop = async () => {
+    console.log('Syncing content files from git')
+
+    await syncRun()
+
+    if (wasCancelled) return
+
+    syncInterval = setTimeout(syncLoop, 1000 * 60)
+  }
+
+  // Block until the first sync is done
+  await syncLoop()
+
+  return () => {
+    wasCancelled = true
+    clearTimeout(syncInterval)
+  }
+}
+
+const runBashCommand = (command: string) =>
+  new Promise((resolve, reject) => {
+    const child = spawn(command, [], { shell: true })
+
+    child.stdout.setEncoding('utf8')
+    child.stdout.on('data', data => process.stdout.write(data))
+
+    child.stderr.setEncoding('utf8')
+    child.stderr.on('data', data => process.stderr.write(data))
+
+    child.on('close', function (code) {
+      if (code === 0) {
+        resolve(void 0)
+      } else {
+        reject(new Error(`Command failed with exit code ${code}`))
+      }
+    })
+  })
+
 export default makeSource({
-  contentDirPath: 'contentlayer',
+  syncFiles: syncContentFromGit,
+  contentDirPath: 'content',
   documentTypes: [Post],
   mdx: {
     remarkPlugins: [remarkGfm, remarkMath, remarkBreaks, remarkHint],
