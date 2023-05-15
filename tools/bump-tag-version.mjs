b033de1fe42415e07778b58a5648ab6ed2e3b1bf
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-05-15 14:56:06 +0000

    chore(tools): rename `.ts` to `.mjs``

--- tools/bump-tag-version.mjs
+++ tools/bump-tag-version.mjs
@@ -1,16 +1,16 @@
 import semver from 'semver'
-import { simpleGit, SimpleGit } from 'simple-git'
+import { simpleGit } from 'simple-git'
 import packageJson from '../package.json'
 import path from 'path'
 
-function log(text: string) {
+function log(text) {
   console.log(`LOG | ${text}`)
 }
 
 const REPO_DIR = path.resolve(__dirname, '../')
 
 async function main() {
-  const simplegit: SimpleGit = simpleGit(REPO_DIR)
+  const simplegit = simpleGit(REPO_DIR)
 
   const tags = await simplegit.tags()
   const lastTag = tags.latest
