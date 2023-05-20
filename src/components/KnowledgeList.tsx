fd241323e5e5678be8beaeb1de8ba6223b35e5d7
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-05-20 16:38:31 +0000

    feat(ui): separate posts by status on post list

--- src/components/KnowledgeList.tsx
+++ src/components/KnowledgeList.tsx
@@ -1,6 +1,7 @@
 import { Post } from 'contentlayer/generated'
 import { categorizePostsByYear } from 'lib/categorizePostsByYear'
-import { KnowledgeLink } from './KnowledgeLink'
+import { PostLink } from './PostLink'
+import { separatePostsByStatus } from 'lib/separatePostsByStatus'
 
 interface Props {
   posts: Post[]
@@ -8,30 +9,48 @@ interface Props {
 }
 
 export function KnowledgeList({ posts, separateByYear = false }: Props) {
+  const postsByStatus = separatePostsByStatus(posts)
+  const testPosts = posts.filter(post => post.test)
+
   if (separateByYear) {
-    const postsByYear = categorizePostsByYear(posts)
+    const postsByYear = categorizePostsByYear([...postsByStatus.publishedPosts, ...postsByStatus.draftPosts])
 
     return (
       <div className="flex flex-col gap-7">
         {postsByYear.map(postsOfYear => (
           <div key={postsOfYear.year}>
             <h1 className="mb-5 rounded-xl bg-neutral-50 p-3 text-2xl font-bold dark:bg-neutral-900">
-              {postsOfYear.year === 1 ? 'Posts for test' : postsOfYear.year}
+              {postsOfYear.year}
             </h1>
             <div className="flex flex-col gap-3">
               {postsOfYear.posts.map((post, key) => (
-                <KnowledgeLink key={key} post={post} hideYear />
+                <PostLink key={key} post={post} hideYear />
               ))}
             </div>
           </div>
         ))}
+        <div >
+          <h1 className="mb-5 rounded-xl bg-neutral-50 p-3 text-2xl font-bold dark:bg-neutral-900">
+            Others
+          </h1>
+          <div className="flex flex-col gap-3">
+            {postsByStatus.plannedPosts.map((post, key) => (
+              <PostLink key={key} post={post} />
+            ))}
+            {
+              testPosts.map((post, key) => (
+                <PostLink key={key} post={post} />
+              ))
+            }
+          </div>
+        </div>
       </div>
     )
   } else {
     return (
       <div className="flex flex-col gap-5">
         {posts.map((post, key) => (
-          <KnowledgeLink key={key} post={post} />
+          <PostLink key={key} post={post} />
         ))}
       </div>
     )
