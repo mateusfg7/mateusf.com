d4e35059c28eaa82657affe74ca5febefa1f2800
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-05-25 13:38:05 +0000

    feat(tailwind): add new animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,9 @@ module.exports = {
         cursor: 'cursor .6s linear infinite alternate',
         typing: 'typing 1.8s ease-in .8s 1 normal both',
         slideDown: 'slideDown 300ms cubic-bezier(0.87, 0, 0.13, 1)',
-        slideUp: 'slideUp 300ms cubic-bezier(0.87, 0, 0.13, 1)'
+        slideUp: 'slideUp 300ms cubic-bezier(0.87, 0, 0.13, 1)',
+        overlayShow: 'overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+        contentShow: 'contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)'
       },
       keyframes: {
         typing: {
@@ -38,11 +40,17 @@ module.exports = {
         slideUp: {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: 0 }
+        },
+        overlayShow: {
+          from: { opacity: 0 },
+          to: { opacity: 1 }
+        },
+        contentShow: {
+          from: { opacity: 0, transform: 'translate(-50%, -48%) scale(0.96)' },
+          to: { opacity: 1, transform: 'translate(-50%, -50%) scale(1)' }
         }
       }
     }
   },
-  plugins: [
-    require('tailwindcss-animated')
-  ]
+  plugins: [require('tailwindcss-animated')]
 }
