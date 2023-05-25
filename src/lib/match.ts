543babb3289f6d000c03130dc6622833139a99a4
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-05-25 13:39:29 +0000

    feat(lib): create `searchMatch` function to be used as search algorithm

---
+++ src/lib/match.ts
@@ -0,0 +1,15 @@
+import { Post } from 'contentlayer/generated'
+import { slug } from './utils'
+
+export function searchMath(post: Post, search: string) {
+  const sluggedSearch = slug(search)
+
+  if (slug(post.title).includes(sluggedSearch)) return true
+  if (slug(post.description).includes(sluggedSearch)) return true
+  if (slug(post.author).includes(sluggedSearch)) return true
+  if (slug(post.category).includes(sluggedSearch)) return true
+  if (slug(post.status).includes(sluggedSearch)) return true
+  if (slug(post.tags).includes(sluggedSearch)) return true
+
+  return false
+}
