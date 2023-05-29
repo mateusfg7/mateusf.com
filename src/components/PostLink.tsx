00b2818a070814fdbe4e8cd005f20233fccd3a9d
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-05-29 13:47:16 +0000

    feat(components): use `@/*` import path on `PostLink` component

--- src/components/PostLink.tsx
+++ src/components/PostLink.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import Link from 'next/link'
 
-import { Date } from './Date'
+import { Date } from '@/components/Date'
 import { Post } from 'contentlayer/generated'
-import { DraftBadge, PlannedBadge, TestBadge } from './PostBadges'
+import { DraftBadge, PlannedBadge, TestBadge } from '@/components/PostBadges'
 
 interface Props {
   post: Post
@@ -14,33 +14,36 @@ export function PostLink({ post, hideYear = false }: Props) {
   const showDate = !post.test && post.status !== 'planned'
 
   if (post.status === 'planned') {
-
     return (
-      <section className="space-y-2 group cursor-pointer">
-        <div className='flex items-center justify-start gap-2'>
+      <section className="group cursor-pointer space-y-2">
+        <div className="flex items-center justify-start gap-2">
           <h2 className="text-xl font-bold text-neutral-700 dark:text-neutral-500">
             {post.title}
           </h2>
-          <span className='group-hover:animate-wiggle group-hover:animate-infinite'>
+          <span className="group-hover:animate-wiggle group-hover:animate-infinite">
             <PlannedBadge />
           </span>
         </div>
         <span className="text-neutral-500">
-          {
-            showDate && (<span><Date dateString={post.date} dateFormat={hideYear ? 'LLL d' : 'LLL d, yyyy'} /> &#8226; </span>)
-          }
+          {showDate && (
+            <span>
+              <Date
+                dateString={post.date}
+                dateFormat={hideYear ? 'LLL d' : 'LLL d, yyyy'}
+              />{' '}
+              &#8226;{' '}
+            </span>
+          )}
 
           {post.description}
         </span>
       </section>
     )
-  }
-  else {
-
+  } else {
     return (
       <Link href={`/post/${post.id}`} className="group">
         <section className="space-y-2">
-          <div className='flex items-center justify-start gap-2'>
+          <div className="flex items-center justify-start gap-2">
             <h2 className="text-xl font-bold text-neutral-700 group-hover:text-blue-700 dark:text-neutral-500 dark:group-hover:text-blue-500">
               {post.title}
             </h2>
@@ -48,9 +51,15 @@ export function PostLink({ post, hideYear = false }: Props) {
             {post.status === 'draft' && !post.test && <DraftBadge />}
           </div>
           <span className="text-neutral-500 group-hover:text-neutral-700 dark:group-hover:text-neutral-400">
-            {
-              showDate && (<span><Date dateString={post.date} dateFormat={hideYear ? 'LLL d' : 'LLL d, yyyy'} /> &#8226; </span>)
-            }
+            {showDate && (
+              <span>
+                <Date
+                  dateString={post.date}
+                  dateFormat={hideYear ? 'LLL d' : 'LLL d, yyyy'}
+                />{' '}
+                &#8226;{' '}
+              </span>
+            )}
 
             {post.description}
           </span>
@@ -58,5 +67,4 @@ export function PostLink({ post, hideYear = false }: Props) {
       </Link>
     )
   }
-
 }
