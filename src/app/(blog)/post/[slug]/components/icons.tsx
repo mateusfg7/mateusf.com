e90fdf0b81e8ac0f06f3140f7b48a86c12efdc5f
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-05-30 14:10:16 +0000

    feat(ui): add reading time on post page

---
+++ src/app/(blog)/post/[slug]/components/icons.tsx
@@ -0,0 +1,8 @@
+'use client'
+
+import { Folder as FolderIcon, IconProps } from '@phosphor-icons/react'
+import React from 'react'
+
+export const Folder: React.FC<IconProps> = ({ ...props }) => (
+  <FolderIcon {...props} />
+)
