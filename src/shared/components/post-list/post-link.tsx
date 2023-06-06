6b69cf6d3c3c86bc6e9d5c91158a306864f297fb
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-06-06 11:28:49 -0300

    feat(ui): move metadata to right on post link component

--- src/shared/components/post-list/post-link.tsx
+++ src/shared/components/post-list/post-link.tsx
@@ -43,42 +43,48 @@ export function PostLink({ post, hideYear = false }: Props) {
       </section>
     )
   } else {
-    return (
-      <Link href={`/post/${post.id}`} className="group">
-        <section className="space-y-1">
-          <div className="flex items-center justify-start gap-2">
-            <h2 className="text-xl font-bold text-neutral-700 group-hover:text-blue-700 dark:text-neutral-500 dark:group-hover:text-blue-500">
-              {post.title}
-            </h2>
+    const Metadata = () => (
+      <span className="group-hover:text-neutral-900dark:group-hover:text-neutral-300 flex flex-col items-end justify-center gap-2 whitespace-nowrap rounded-lg p-1 leading-none text-neutral-600 group-hover:text-neutral-700 dark:group-hover:text-neutral-400">
+        {showDate && (
+          <span className="inline-flex items-center gap-1">
+            <Date
+              dateString={post.date}
+              dateFormat={hideYear ? 'LLL d' : 'LLL d, yyyy'}
+            />
+            <CalendarBlank />
+          </span>
+        )}
 
-            {post.test && <TestBadge />}
-            {post.status === 'draft' && !post.test && <DraftBadge />}
+        {showReadingTime && (
+          <span className="inline-flex items-center gap-1">
+            <span>{Math.ceil(post.reading_time.minutes)} min read</span>
+            <Clock />
+          </span>
+        )}
+      </span>
+    )
+
+    return (
+      <Link
+        href={`/post/${post.id}`}
+        className="group flex justify-between gap-3"
+      >
+        <section className="space-y-1">
+          <div className="flex items-center justify-between gap-2">
+            <div className="flex items-center justify-start gap-2">
+              <h2 className="text-xl font-bold text-neutral-700 group-hover:text-blue-700 dark:text-neutral-500 dark:group-hover:text-blue-500">
+                {post.title}
+              </h2>
+
+              {post.test && <TestBadge />}
+              {post.status === 'draft' && !post.test && <DraftBadge />}
+            </div>
           </div>
           <span className="inline-flex items-center gap-3 text-neutral-500 group-hover:text-neutral-700 dark:group-hover:text-neutral-400">
-            {(showDate || showReadingTime) && (
-              <span className="inline-flex items-center gap-2 rounded-lg bg-neutral-200/40 p-1 leading-none dark:bg-neutral-800/30">
-                {showDate && (
-                  <span className="inline-flex items-center gap-1">
-                    <CalendarBlank />
-                    <Date
-                      dateString={post.date}
-                      dateFormat={hideYear ? 'LLL d' : 'LLL d, yyyy'}
-                    />
-                  </span>
-                )}
-
-                {showReadingTime && (
-                  <span className="inline-flex items-center gap-1">
-                    <Clock />
-                    <span>{Math.ceil(post.reading_time.minutes)} min read</span>
-                  </span>
-                )}
-              </span>
-            )}
-
             {post.description}
           </span>
         </section>
+        {(showDate || showReadingTime) && <Metadata />}
       </Link>
     )
   }
