e37eee6cade7c907ae9e8def05ceac0421de29dc
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-06-13 18:21:00 -0300

    feat(lib): create `getTagsAndNumberOfPosts` function

--- src/shared/lib/tags.ts
+++ src/shared/lib/tags.ts
@@ -1,6 +1,7 @@
 import { allPosts } from 'contentlayer/generated'
 import { slug } from '@/shared/lib/slug'
 import { removeRepeatedValuesFromArray } from '@/shared/lib/remove-repeated-values-from-array'
+import { getFrequencyOfValue } from '@/shared/lib/get-frequency-of-value'
 
 const getRawTagListFromPosts = (): string[] => {
   const listOfTagList = allPosts
@@ -20,6 +21,26 @@ const getRawTagListFromPosts = (): string[] => {
 
 export const getUniqueTagListFromPosts = () =>
   removeRepeatedValuesFromArray(getRawTagListFromPosts())
+
+export interface TagsAndNumberOfPosts {
+  tag: string
+  numberOfPosts: number
+}
+export function getTagsAndNumberOfPosts(): TagsAndNumberOfPosts[] {
+  const rawTagList = getRawTagListFromPosts()
+  const uniqueTagList = getUniqueTagListFromPosts()
+
+  const tagsAndNumberOfPosts = uniqueTagList.map(tag => {
+    const numberOfPosts = getFrequencyOfValue(rawTagList, tag)
+
+    return {
+      tag,
+      numberOfPosts
+    }
+  })
+
+  return tagsAndNumberOfPosts
+}
 
 export function getPostListBasedOnTag(tag: string) {
   const filteredPostList = allPosts.filter(post => {
