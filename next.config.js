914ea4d2ca04f6c9d2de51e2a1b7a55cf8b9e91c
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-06-16 10:20:11 -0300

    chore(next): add some redirects to next config

--- next.config.js
+++ next.config.js
@@ -6,6 +6,30 @@ const nextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true
   },
+  async redirects() {
+    return [
+      {
+        source: '/rss',
+        destination: '/feed',
+        permanet: true
+      },
+      {
+        source: '/atom',
+        destination: '/feed',
+        permanet: true
+      },
+      {
+        source: '/rss.xml',
+        destination: '/feed',
+        permanet: true
+      },
+      {
+        source: '/atom.xml',
+        destination: '/feed',
+        permanet: true
+      },
+    ]
+  }
 }
 
 module.exports = withContentlayer(nextConfig)
