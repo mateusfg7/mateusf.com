2d1e04c9519b84b132e985cfe58037c8f11c88e4
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-07-04 18:23:58 -0300

    feat(seo): add post thumbnail path to robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
-      allow: '/'
+      allow: ['/', '/post/*/thumbnail'],
     },
     sitemap: `${host}/sitemap.xml`
   }
