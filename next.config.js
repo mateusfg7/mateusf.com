5d13a47d76db81c3e67d75de00bfb190bab8033b
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-07-10 12:12:41 -0300

    chore(next): remove eslint configs

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 const { withContentlayer } = require('next-contentlayer')
 
 const nextConfig = {
-  eslint: {
-    // Warning: Dangerously allow production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true
-  },
   async rewrites() {
     return [
       {
