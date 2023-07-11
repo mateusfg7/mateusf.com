80d1b1c2113e707ffd012b812f4935a3f21919eb
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-07-11 11:23:49 -0300

    chore: re-setup prettier and eslint

--- prettier.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: [require('prettier-plugin-tailwindcss')],
-  semi: false,
-  singleQuote: true,
-  arrowParens: 'avoid',
-  trailingComma: 'none',
-  endOfLine: 'auto'
-}
