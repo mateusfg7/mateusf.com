46a7332bfc15f567191ec95a3d6dda9917038964
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-07-13 16:59:52 -0300

    feat(providers): add kbar provider on providers list

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,11 +3,14 @@
 import { ReactNode } from 'react'
 import { ThemeProvider } from 'next-themes'
 import { GeistProvider } from '@geist-ui/core'
+import { CustomKBarProvider } from '@/shared/components/kbar/provider'
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <GeistProvider>{children}</GeistProvider>
+      <CustomKBarProvider>
+        <GeistProvider>{children}</GeistProvider>
+      </CustomKBarProvider>
     </ThemeProvider>
   )
 }
