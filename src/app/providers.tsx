ec72544d70c5e1e17e0347c6b3046cc21e301826
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-07-13 17:40:08 -0300

    refactor(clean): remove unused geist-ui provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,15 +2,12 @@
 
 import { ReactNode } from 'react'
 import { ThemeProvider } from 'next-themes'
-import { GeistProvider } from '@geist-ui/core'
 import { CustomKBarProvider } from '@/shared/components/kbar/provider'
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <CustomKBarProvider>
-        <GeistProvider>{children}</GeistProvider>
-      </CustomKBarProvider>
+      <CustomKBarProvider>{children}</CustomKBarProvider>
     </ThemeProvider>
   )
 }
