537c64f3893b64758b6d6398b2c4a7b5298cc95d
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-07-13 17:01:21 -0300

    refactor(clean): delete old search modal

--- src/app/(blog)/components/header/search/overlay.tsx
+++
@@ -1,5 +0,0 @@
-import * as Dialog from '@radix-ui/react-dialog'
-
-export const Overlay = () => (
-  <Dialog.Overlay className="fixed inset-0 bg-neutral-50/50 data-[state=open]:animate-overlayShow dark:bg-black/50" />
-)
