feb091ddc0b8e00367d49ac11432596bb57c9d74
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-07-17 17:17:37 -0300

    feat(ui): create home page

---
+++ src/app/(main)/components/start-button.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { useKBar } from 'kbar'
+import { ArrowRight } from '@/shared/lib/phosphor-icons'
+
+export function StartButton() {
+  const {
+    query: { toggle }
+  } = useKBar()
+
+  return (
+    <button
+      onClick={toggle}
+      className="group flex items-center gap-1 text-sm tracking-[.2em] opacity-60 hover:opacity-100 active:opacity-100"
+    >
+      <span className="hidden md:inline">Press</span>
+      <kbd className="keyboard hidden tracking-normal opacity-90 md:inline">
+        Ctrl K
+      </kbd>
+      <span className="inline md:hidden">Click</span>
+      <span>to start</span>
+
+      <ArrowRight className="duration-300 group-hover:translate-x-0 group-hover:opacity-100 md:-translate-x-full md:opacity-0" />
+    </button>
+  )
+}
---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image'
+import { Typewriter } from './components/typewriter'
+import codeTyping from './components/code-type.svg'
+import { StartButton } from './components/start-button'
+
+export default function Page() {
+  return (
+    <div className="blog-content-w relative m-auto mb-10 flex min-h-screen flex-col items-center justify-between py-10">
+      <div className="absolute h-full w-full bg-[url(/assets/code-type.svg)] bg-fixed bg-center bg-no-repeat opacity-10 md:hidden" />
+      <div />
+      <div className="flex w-full items-center justify-center gap-12 md:justify-between">
+        <div className="flex flex-col gap-4">
+          <span className="w-min text-7xl font-bold text-neutral-800 dark:text-neutral-50 md:w-max">
+            Mateus Felipe,
+          </span>
+          <span className="flex items-center text-2xl text-neutral-400 md:text-3xl ">
+            <Typewriter
+              words={[
+                'Front-end developer',
+                'Back-end developer',
+                'Full stack developer'
+              ]}
+              cursor
+            />
+          </span>
+        </div>
+        <div className="hidden w-64 flex-1 md:block">
+          <Image src={codeTyping} alt="Code Typing" className="w-full" />
+        </div>
+      </div>
+      <StartButton />
+    </div>
+  )
+}
