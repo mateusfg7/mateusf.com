36dfa67c37bbfe53d2bfd7dccc7cb5e2cade1153
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-07-20 20:04:50 -0300

    feat(ui): add title to blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -17,6 +17,9 @@ export default function Page() {
 
   return (
     <div className="content-container m-auto">
+      <h1 className="mb-8 bg-gradient-to-br from-blue-700 to-blue-400 bg-clip-text text-center text-4xl font-bold text-transparent md:w-fit md:text-left">
+        Blog
+      </h1>
       <PostList posts={posts} separateByYear />
     </div>
   )
