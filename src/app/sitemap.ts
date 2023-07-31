1987d65289357bb6853fd59dc031f744434c846a
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-07-31 09:09:51 -0300

    refactor(sitemap): create separated array with all routes

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,46 +1,9 @@
 import { MetadataRoute } from 'next'
-import { getUniqueCategoryList } from '@/shared/lib/categories'
-import { getUniqueTagListFromPosts } from '@/shared/lib/tags'
-import { config } from 'global-config'
-import { allPosts } from 'contentlayer/generated'
-import { slug } from '@/shared/lib/slug'
+import { allRoutes } from './lib/all-routes'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const commonRoutes = [
-    '',
-    'about',
-    'projects',
-    'blog',
-    'blog/categories',
-    'blog/tag',
-    'blog/feed',
-    'blog/author'
-  ].map(route => ({
-    url: `${config.webserver.host}/${route}`,
+  return allRoutes.map(url => ({
+    url,
     lastModified: new Date().toISOString()
   }))
-
-  const tags = getUniqueTagListFromPosts().map(tag => ({
-    url: `${config.webserver.host}/blog/tag/${slug(tag)}`,
-    lastModified: new Date().toISOString()
-  }))
-  const categories = getUniqueCategoryList().map(category => ({
-    url: `${config.webserver.host}/blog/categories/${slug(category)}`,
-    lastModified: new Date().toISOString()
-  }))
-  const posts = allPosts.map(post => ({
-    url: `${config.webserver.host}/blog/post/${post.id}`,
-    lastModified: new Date(post.date).toISOString()
-  }))
-
-  return [
-    ...commonRoutes,
-    ...tags,
-    ...categories,
-    ...posts,
-    {
-      url: `${config.webserver.host}/blog/author/mateusfg7`,
-      lastModified: new Date().toISOString()
-    }
-  ]
 }
