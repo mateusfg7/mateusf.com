0cd691a8f6b68a7e3763bececdcff90db8b9dd93
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-10-28 18:37:52 -0300

    feat(ui): update homepage illustration
    fix #679

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,18 @@ import Image from 'next/image'
 
 import { Typewriter } from './components/typewriter'
 import { StartButton } from './components/start-button'
-import codeTyping from './components/code-type.svg'
+
+import deconstructedRobotBroLight from './components/deconstructed-robot-bro-light.svg'
+import deconstructedRobotBroDark from './components/deconstructed-robot-bro-dark.svg'
 
 export default function Page() {
   return (
     <div className="content-container relative m-auto mb-10 flex min-h-screen flex-col items-center justify-between py-10">
-      <div className="absolute h-full w-full bg-[url(/assets/code-type.svg)] bg-fixed bg-center bg-no-repeat opacity-10 md:hidden" />
+      <div className="absolute h-full w-full bg-[url(/assets/deconstructed-robot-bro.svg)] bg-fixed bg-center bg-no-repeat opacity-10 dark:opacity-5 md:hidden" />
       <div />
       <div className="flex w-full items-center justify-center gap-12 md:justify-between">
         <div className="flex flex-col gap-4">
-          <span className="w-min text-7xl font-bold text-neutral-800 dark:text-neutral-50 md:w-max">
+          <span className="w-min text-7xl font-bold text-black dark:text-neutral-50 md:w-max">
             Mateus Felipe,
           </span>
           <span className="flex items-center text-2xl text-neutral-400 md:text-3xl ">
@@ -25,8 +27,17 @@ export default function Page() {
             />
           </span>
         </div>
-        <div className="hidden w-64 flex-1 md:block">
-          <Image src={codeTyping} alt="Code Typing" className="w-full" />
+        <div className="hidden flex-1 items-center justify-end md:flex">
+          <Image
+            src={deconstructedRobotBroLight}
+            alt="Deconstructed Robot Light"
+            className="w-72 dark:hidden"
+          />
+          <Image
+            src={deconstructedRobotBroDark}
+            alt="Deconstructed Robot Dark"
+            className="hidden w-72 dark:block"
+          />
         </div>
       </div>
       <StartButton />
