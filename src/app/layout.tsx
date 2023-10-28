078774f3aa699e0ffcd5887184b5e13874dc4acf
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-10-28 18:38:22 -0300

    feat(ui): add bottom-right illustrantion on all pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,6 +58,8 @@ export default function RootLayout({
         <Providers>
           <Header />
           <div>{children}</div>
+
+          <div className="fixed bottom-0 right-0 -z-50 hidden h-96 w-96 bg-[url(/assets/server-status-bro-light.svg)] bg-contain bg-bottom bg-no-repeat opacity-10 dark:bg-[url(/assets/server-status-bro-dark.svg)] dark:opacity-5 md:block " />
           <Footer />
         </Providers>
       </body>
