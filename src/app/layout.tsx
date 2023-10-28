7baeb321de223d4ac579d2254fd221e80a562d74
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-10-28 18:57:39 -0300

    feat(ui): add top-left illustrantion on all pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,6 +59,8 @@ export default function RootLayout({
           <Header />
           <div>{children}</div>
 
+          <div className="fixed left-0 top-10 -z-50 hidden h-64 w-64 bg-[url(/assets/robin-bird-bro.svg)] bg-contain bg-bottom bg-no-repeat opacity-10                                         dark:opacity-5 md:block " />
+
           <div className="fixed bottom-0 right-0 -z-50 hidden h-96 w-96 bg-[url(/assets/server-status-bro-light.svg)] bg-contain bg-bottom bg-no-repeat opacity-10 dark:bg-[url(/assets/server-status-bro-dark.svg)] dark:opacity-5 md:block " />
           <Footer />
         </Providers>
