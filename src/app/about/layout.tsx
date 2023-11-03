7f96b27221d93ce2e9e42d90c200a05e3fd6b04f
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-11-03 12:22:48 +0000

    perf: add `revalidate` config on `/about` page layout
    Revalidate data in every 3600 seconds (1 hour)

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -7,6 +7,8 @@ export const metadata: Metadata = {
   keywords: ['about', 'social', 'links', 'knowledge']
 }
 
+export const revalidate = 3600 // 3600s = 1h
+
 export default function AboutLayout({
   children
 }: {
