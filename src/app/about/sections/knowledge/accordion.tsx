18bf9e648363d1d1fdae73557b63f3bd0ef31adf
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-11-03 17:16:10 +0000

    refactor: move `Dot` component out of `KnowledgeInfo`

--- src/app/about/sections/knowledge/accordion.tsx
+++ src/app/about/sections/knowledge/accordion.tsx
@@ -65,16 +65,15 @@ export const AccordionContent = React.forwardRef(
   )
 )
 
-export function KnowledgeInfo({ knowledge }: { knowledge: Knowledge }) {
-  const Dot = () => (
-    <span className="absolute right-2 top-2">
-      <span className="relative flex h-1 w-1">
-        <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75"></span>
-        <span className="relative inline-flex h-1 w-1 rounded-full bg-green-500"></span>
-      </span>
+const Dot = () => (
+  <span className="absolute right-2 top-2">
+    <span className="relative flex h-1 w-1">
+      <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75"></span>
+      <span className="relative inline-flex h-1 w-1 rounded-full bg-green-500"></span>
     </span>
-  )
-
+  </span>
+)
+export function KnowledgeInfo({ knowledge }: { knowledge: Knowledge }) {
   return (
     <div
       key={knowledge.title}
