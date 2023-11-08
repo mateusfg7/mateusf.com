d45d8ff72d5b2a5949cb9b12eca384a05395fdad
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-11-08 18:21:28 +0000

    refactor: use `lastFm` lib on `top-artist` card

--- src/app/about/sections/statistics/cards/top-artist.tsx
+++ src/app/about/sections/statistics/cards/top-artist.tsx
@@ -1,33 +1,11 @@
 import { SpotifyLogo } from '@/shared/wrappers/phosphor-icons'
+import { getTopArtists } from '@/shared/lib/lastFm'
 
-import { ApiErrorMessage } from '../api-error'
 import { Card } from '../card'
 
-type Artist = {
-  name: string
-  url: string
-  playcount: string
-}
-
-type Artists = {
-  topartists: {
-    artist: Artist[]
-  }
-}
-
 export async function TopArtist() {
-  const lastFmApiRequest = await fetch(
-    `http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=mateusfg7&api_key=${process.env.LASTFM_API_KEY}&format=json`
-  )
-
-  if (!lastFmApiRequest.ok) {
-    console.log(lastFmApiRequest)
-    return <Card title="Top Artist" content={<ApiErrorMessage />} />
-  }
-
-  const jsonResponse: Artists = await lastFmApiRequest.json()
-
-  const mostListened = jsonResponse.topartists.artist[0]
+  const topArtists = await getTopArtists()
+  const mostListened = topArtists[0]
 
   return (
     <Card
