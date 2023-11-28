97698f40fee08c3e42fbcd7b9aff7bdc1a4813f9
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-11-28 23:46:41 +0000

    feat(content): include `Til` on contentlayer config

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -4,12 +4,13 @@ import { makeSource } from 'contentlayer/source-files'
 
 import { Post } from './content/definitions/Post'
 import { Project } from './content/definitions/Project'
+import { Til } from './content/definitions/Til'
 import { AboutMe } from './content/definitions/AboutMe'
 import { remarkPlugins, rehypePlugins } from './content/plugin'
 
 export default makeSource({
   contentDirPath: 'content',
-  documentTypes: [Post, Project, AboutMe],
+  documentTypes: [Post, Project, Til, AboutMe],
   disableImportAliasWarning: true,
   mdx: {
     remarkPlugins,
