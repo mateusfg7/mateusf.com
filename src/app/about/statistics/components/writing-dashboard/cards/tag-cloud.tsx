7c49e77180b0c7c632ba1972fa8568506c594607
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-12-19 14:55:03 +0000

    feat(ui): update tag cloud implementation

--- src/app/about/statistics/components/writing-dashboard/cards/tag-cloud.tsx
+++ src/app/about/statistics/components/writing-dashboard/cards/tag-cloud.tsx
@@ -1,91 +1,62 @@
-'use client'
+import arrayShuffle from 'shuffle-array'
 
-import ReactWordcloud, { Word, Options, defaultOptions } from 'react-wordcloud'
 import { Tag } from '@/shared/wrappers/phosphor-icons'
 import { getTagsAndNumberOfPosts } from '@/shared/lib/tags'
 
-export async function TagCloud() {
-  // const words: Word[] = [
-  //   {
-  //     text: 'mateus',
-  //     value: 10
-  //   },
-  //   {
-  //     text: 'rust',
-  //     value: 10
-  //   },
-  //   {
-  //     text: 'deno',
-  //     value: 6
-  //   },
-  //   {
-  //     text: 'bun',
-  //     value: 6
-  //   },
-  //   {
-  //     text: 'node',
-  //     value: 6
-  //   },
-  //   {
-  //     text: 'next.js',
-  //     value: 6
-  //   },
-  //   {
-  //     text: 'felipe',
-  //     value: 4
-  //   },
-  //   {
-  //     text: 'svelte',
-  //     value: 4
-  //   },
-  //   {
-  //     text: 'vue',
-  //     value: 4
-  //   },
-  //   {
-  //     text: 'nest.js',
-  //     value: 2
-  //   },
-  //   {
-  //     text: 'python',
-  //     value: 2
-  //   }
-  // ]
+// https://github.com/madox2/react-tagcloud/blob/43a706b36a606ddd8a39c4060510166aebdf445e/src/helpers.js#L4
+const fontSizeConverter = (
+  count: number,
+  lowestCount: number,
+  highestCount: number,
+  minSizePx: number,
+  maxSizePx: number
+) => {
+  if (highestCount - lowestCount === 0) {
+    // handle devision by zero
+    return Math.round((minSizePx + maxSizePx) / 2)
+  }
+  return Math.round(
+    ((count - lowestCount) * (maxSizePx - minSizePx)) /
+      (highestCount - lowestCount) +
+      minSizePx
+  )
+}
 
-  const words: Word[] = getTagsAndNumberOfPosts().map(value => ({
-    text: value.tag,
-    value: value.numberOfPosts
-  }))
+export async function TagCloud() {
+  const tags: { value: string; count: number }[] = arrayShuffle(
+    getTagsAndNumberOfPosts().map(value => ({
+      value: value.tag,
+      count: value.numberOfPosts
+    }))
+  )
 
   return (
-    <div className="flex h-min w-full flex-col justify-center gap-3 rounded-3xl bg-neutral-200 p-4 leading-none dark:bg-neutral-950 md:p-7">
+    <div className="flex h-full w-full flex-col justify-center gap-3 rounded-3xl bg-neutral-200 p-4 leading-none dark:bg-neutral-950 md:p-7">
       <span className="inline-flex items-center gap-2 text-neutral-600">
         <span>Most Used Tags</span>
         <Tag weight="duotone" />
       </span>
-      <div className="flex items-center text-2xl">
-        <div className="dark:hidden">
-          <ReactWordcloud
-            words={words}
-            options={{
-              rotationAngles: [0, 0],
-              rotations: 0,
-              padding: 5,
-              colors: ['#333']
+      <div className="flex h-full flex-wrap gap-1 leading-none">
+        {tags.map(tag => (
+          <span
+            key={tag.value}
+            style={{
+              fontSize: fontSizeConverter(
+                tag.count,
+                Math.min(...tags.map(tag => tag.count)),
+                Math.max(...tags.map(tag => tag.count)),
+                9,
+                30
+              ),
+              opacity: Math.max(
+                tag.count / Math.max(...tags.map(tag => tag.count)),
+                0.8
+              )
             }}
-          />
-        </div>
-        <div className="hidden h-full w-full dark:block">
-          <ReactWordcloud
-            words={words}
-            options={{
-              rotationAngles: [0, 0],
-              rotations: 0,
-              padding: 5,
-              colors: ['#666']
-            }}
-          />
-        </div>
+          >
+            {tag.value}
+          </span>
+        ))}
       </div>
     </div>
   )
