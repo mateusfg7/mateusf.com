ec0e261d656af39b1697137615ef5c589dd10484
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-12-30 16:38:00 -0300

    feat(ui): add description on contribution graph

--- src/app/about/statistics/components/github-dashboard/cards/line-graph/index.tsx
+++ src/app/about/statistics/components/github-dashboard/cards/line-graph/index.tsx
@@ -7,10 +7,13 @@ export async function LineGraph() {
 
   return (
     <div className="flex h-full w-full flex-col justify-center gap-3 rounded-3xl bg-neutral-200 p-4 leading-none dark:bg-neutral-950 md:p-7">
-      <span className="inline-flex items-center gap-2 text-neutral-600">
-        <span>Contribution Graph</span>
-        <ChartLine size="1em" weight="duotone" />
-      </span>
+      <div className="flex flex-col leading-none">
+        <span className="inline-flex items-center gap-2 text-neutral-600">
+          <span>Contribution Graph</span>
+          <ChartLine size="1em" weight="duotone" />
+        </span>
+        <span className="text-xs text-neutral-600/50">from last 31 days</span>
+      </div>
       <div className="flex h-full items-center">
         <Chart data={githubActivity.contributions} />
       </div>
