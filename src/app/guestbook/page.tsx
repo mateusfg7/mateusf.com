9e170a65e318da1f8a98af89cd86f756fa69d8e1
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-01-23 12:51:24 +0000

    feat(page): create `/guestbook` page

---
+++ src/app/guestbook/page.tsx
@@ -0,0 +1,33 @@
+import { GiscusComments } from '@/shared/components/giscus-comments'
+import { Title } from '@/shared/components/title'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Guestbook',
+  description:
+    'A guestbook where you can leave your precious message for me and also see all old messages',
+  openGraph: {
+    title: 'Guestbook',
+    description:
+      'A guestbook where you can leave your precious message for me and also see all old messages'
+  }
+}
+
+export default function Page() {
+  return (
+    <div className="content-vertical-spaces content-container m-auto space-y-10 md:space-y-4">
+      <Title text="Guestbook" />
+      <div>
+        <GiscusComments
+          category="Show and tell"
+          categoryId="MDE4OkRpc2N1c3Npb25DYXRlZ29yeTMyNzg0MzYw"
+          reactionsEnabled="0"
+          inputPosition="top"
+          mapping="specific"
+          term="Guestbook"
+          transparentDark={false}
+        />
+      </div>
+    </div>
+  )
+}
