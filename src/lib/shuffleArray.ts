b2ae59d821344ff5d1ca3b7e8ff8623c0dca5ca7
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-02-13 12:19:02 -0300

    feat(shuffle-array): check array type on parameter level

--- src/lib/shuffleArray.ts
+++ src/lib/shuffleArray.ts
@@ -1,6 +1,4 @@
-export function shuffleArray<T>(array: T) {
-  if (!Array.isArray(array)) throw new Error('"array" is not an array')
-
+export function shuffleArray<T>(array: T[]) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1))
     ;[array[i], array[j]] = [array[j], array[i]]
