37ce8d02dc9017ecb657c64cb163ffd1ac3be7c3
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-02-27 14:51:41 +0000

    chore(next): remove velite plugin

--- next.config.js
+++ next.config.js
@@ -1,23 +1,3 @@
-class VeliteWebpackPlugin {
-  static started = false
-  constructor(/** @type {import('velite').Options} */ options = {}) {
-    this.options = options
-  }
-  apply(/** @type {import('webpack').Compiler} */ compiler) {
-    // executed three times in nextjs !!!
-    // twice for the server (nodejs / edge runtime) and once for the client
-    compiler.hooks.beforeCompile.tapPromise('VeliteWebpackPlugin', async () => {
-      if (VeliteWebpackPlugin.started) return
-      VeliteWebpackPlugin.started = true
-      const dev = compiler.options.mode === 'development'
-      this.options.watch = this.options.watch ?? dev
-      this.options.clean = this.options.clean ?? !dev
-      const { build } = await import('velite')
-      await build(this.options) // start velite
-    })
-  }
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -59,10 +39,6 @@ const nextConfig = {
         headers: [{ key: 'Access-Control-Allow-Origin', value: '*' }]
       }
     ]
-  },
-  webpack: config => {
-    config.plugins.push(new VeliteWebpackPlugin())
-    return config
   }
 }
 
