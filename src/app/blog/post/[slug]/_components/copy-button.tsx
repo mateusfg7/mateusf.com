810c436fb96fe19114eaeee491a4ea1b2c83d3b1
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-02-28 14:09:56 +0000

    feat(blog): improve codeblock styles

--- src/app/blog/post/[slug]/_components/copy-button.tsx
+++ src/app/blog/post/[slug]/_components/copy-button.tsx
@@ -19,7 +19,7 @@ export const CopyButton = ({ text }) => {
     <button
       disabled={isCopied}
       onClick={copy}
-      className={`absolute bottom-0 right-0 flex items-center gap-1 rounded p-2 leading-none   hover:opacity-100 active:opacity-100 ${
+      className={`absolute bottom-0 right-0 flex items-center gap-1 rounded-tl-lg p-2 leading-none hover:opacity-100 active:opacity-100 ${
         isCopied
           ? 'bg-green-100/40 text-green-800 opacity-100 dark:bg-green-300/10 dark:text-green-500'
           : 'cursor-pointer bg-neutral-300 text-neutral-600 opacity-40 dark:bg-neutral-800 dark:text-neutral-400'
