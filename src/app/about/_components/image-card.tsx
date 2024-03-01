3c62041721ca12316a5d6095ec2d0e2a4bd065f6
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-03-01 14:38:36 +0000

    feat(ui): update image card style

--- src/app/about/_components/image-card.tsx
+++ src/app/about/_components/image-card.tsx
@@ -5,9 +5,23 @@ import Tilt from 'react-parallax-tilt'
 import { placeholder } from '../../../lib/placeholder'
 
 export function ImageCard({ mobile = false }) {
+  if (mobile) {
+    return (
+      <Image
+        src="https://github.com/mateusfg7.png"
+        height={460}
+        width={460}
+        alt="Mateus Felipe's picture"
+        className="my-5 rounded-3xl border border-neutral-800 object-cover dark:border-neutral-500"
+        placeholder={placeholder(460, 460) as `data:image/${string}`}
+        priority
+      />
+    )
+  }
+
   return (
     <Tilt
-      className="w-fit overflow-hidden rounded-3xl shadow-2xl dark:shadow-black"
+      className="w-fit overflow-hidden rounded-3xl shadow-xl dark:shadow-none"
       tiltMaxAngleX={5}
       tiltMaxAngleY={5}
       glarePosition="all"
@@ -24,9 +38,7 @@ export function ImageCard({ mobile = false }) {
         height={460}
         width={460}
         alt="Mateus Felipe's picture"
-        className={`object-cover dark:brightness-75 ${
-          !mobile && 'h-[30rem] w-96'
-        }`}
+        className="h-[30rem] w-96 object-cover dark:brightness-90"
         placeholder={placeholder(460, 460) as `data:image/${string}`}
         priority
       />
