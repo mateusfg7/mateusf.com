2facd1d40bef9eec1afbad8a1ec139186d975ef2
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-05-17 13:39:07 +0000

    feat(icons): update some icons

--- src/components/icons/github.tsx
+++ src/components/icons/github.tsx
@@ -3,7 +3,7 @@ import { SVGProps } from 'react'
 export function GithubIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg width="1em" height="1em" viewBox="0 0 128 128" {...props}>
-      <g fill="#181616">
+      <g className="fill-[#181616] dark:fill-white">
         <path
           fill-rule="evenodd"
           clip-rule="evenodd"
--- src/components/icons/windows.tsx
+++ src/components/icons/windows.tsx
@@ -1,5 +1,12 @@
 import { SVGProps } from 'react'
 
 export function WindowsIcon(props: SVGProps<SVGSVGElement>) {
-  return <svg width="1em" height="1em" viewBox="0 0 128 128" {...props}></svg>
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 128 128" {...props}>
+      <path
+        fill="#0078d4"
+        d="M67.328 67.331h60.669V128H67.328zm-67.325 0h60.669V128H.003zM67.328 0h60.669v60.669H67.328zM.003 0h60.669v60.669H.003z"
+      ></path>
+    </svg>
+  )
 }
