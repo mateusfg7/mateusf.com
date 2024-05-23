f8baef821bbaa844ffc6db0b69b190ae118acb93
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-05-23 17:43:50 +0000

    feat(home): dark mode on latest-post card

--- src/app/_components/grid/cards/latest-post.tsx
+++ src/app/_components/grid/cards/latest-post.tsx
@@ -8,15 +8,15 @@ export function LatestPost() {
   const latestPost = getSortedPosts(posts)[0]
 
   const Divider = () => (
-    <span className="h-px w-full rounded-full bg-neutral-1000 opacity-20" />
+    <span className="h-px w-full rounded-full bg-neutral-1000 opacity-20 dark:bg-neutral-300/50" />
   )
 
   return (
     <Link
       href={`/blog/post/${latestPost.slug}`}
-      className="relative flex w-full flex-1 transform-gpu flex-col gap-3 overflow-hidden rounded-xl border border-neutral-200 bg-gradient-to-br from-neutral-50 to-neutral-100 p-5 transition-all duration-500 hover:scale-[.97]"
+      className="relative flex w-full flex-1 transform-gpu flex-col gap-3 overflow-hidden rounded-xl border border-neutral-200 bg-gradient-to-br from-neutral-50 to-neutral-100 p-5 transition-all duration-500 hover:scale-[.97] dark:border-neutral-900 dark:from-neutral-1000 dark:to-neutral-950"
     >
-      <div className="absolute left-0 right-0 top-1/2 -z-10 flex -translate-y-1/2 justify-around opacity-30 blur-3xl">
+      <div className="absolute left-0 right-0 top-1/2 -z-10 flex -translate-y-1/2 justify-around opacity-30 blur-3xl dark:opacity-20">
         <div className="size-36 animate-background-fade rounded-xl" />
         <div className="size-36 animate-background-fade rounded-xl animate-delay-200" />
       </div>
