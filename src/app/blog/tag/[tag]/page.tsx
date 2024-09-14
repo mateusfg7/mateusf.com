3f84cf987f5e1047c1addd06168f06513007ece5
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-09-14 16:13:45 -0300

    feat(tags): include TILs on tag pages

--- src/app/blog/tag/[tag]/page.tsx
+++ src/app/blog/tag/[tag]/page.tsx
@@ -11,6 +11,9 @@ import {
 } from '~/lib/tags'
 import { getSortedPosts } from '~/lib/get-sorted-posts'
 import { slug } from '~/lib/slug'
+import { tils } from '#content'
+import { TilList } from '~/components/til-list'
+import { Title } from '~/components/title'
 
 interface Props {
   params: { tag: string }
@@ -27,16 +30,26 @@ export default function Page({ params }: Props) {
   const { tag } = params
 
   const postList = getSortedPosts(getPostListBasedOnTag(slug(tag)))
+  const tilList = tils.filter(til => til.tags.includes(tag))
 
   return (
-    <div className="content-container m-auto">
+    <div className="content-container m-auto space-y-7">
       <h1 className="mb-5 flex items-end justify-center gap-2 text-2xl font-bold md:justify-start">
         {getNormalTagString(tag)}
         <Tag size="1em" weight="duotone" />
       </h1>
-      <main>
-        <PostList posts={postList} />
-      </main>
+      {postList.length > 0 && (
+        <div className="space-y-4">
+          <Title text="Blog Posts" />
+          <PostList posts={postList} />
+        </div>
+      )}
+      {tilList.length > 0 && (
+        <div className="space-y-4">
+          <Title text="T.I.L." />
+          <TilList tils={tilList} />
+        </div>
+      )}
     </div>
   )
 }
