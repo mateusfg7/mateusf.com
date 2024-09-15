2fb7c2967d7848be9d156a9897af728aad898eef
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-09-15 11:57:35 -0300

    feat(lib): improve `slug` parser

--- src/lib/slug.ts
+++ src/lib/slug.ts
@@ -1,4 +1,10 @@
 import { slug as githubSlugger } from 'github-slugger'
 
 export const slug = (text: string) =>
-  githubSlugger(text).toLowerCase().normalize('NFD').replaceAll(/\p{M}/gu, '')
+  githubSlugger(text)
+    .toLowerCase()
+    .normalize('NFD')
+    .replaceAll(/\p{M}/gu, '')
+    .replaceAll(/[^a-z0-9-]/g, '-')
+    .replaceAll(/-+/g, '-')
+    .replaceAll(/^-|-$/g, '')
